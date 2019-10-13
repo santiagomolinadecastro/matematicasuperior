70237ea08170ea1bd342eb1283b3541361aba286
Facundo Yuffrida <fyuffrida@baufest.com>
2019-10-13 11:08:36 -0300

    Se agrega .gitignore para que no suba los archivos dentro de .vs. Se agrega NewtonGregory a la capa de negocio. Se avanza sobre NewtoGregory progresivo.

--- UTN.MatematicaSuperior/UTN.MatematicaSuperior.Consola/Program.cs
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Consola/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UTN.MatematicaSuperior.Negocio;
 
 namespace UTN.MatematicaSuperior.Consola
 {
@@ -10,76 +8,12 @@ namespace UTN.MatematicaSuperior.Consola
     {
         static void Main(string[] args)
         {
-            NewtonGregory();
-        }
+            List<double> xList = new List<double>(), yList = new List<double>();
+            
+            string resultado = NewtonGregory.Interpolar(xList, yList);
 
-        #region - Private Methods -
-
-        static void NewtonGregory()
-        {
-            int n = 5;
-            double[] x = { 45, 50, 55, 60, 70 };
-
-            double[,] y = new double[n, n];
-            y[0, 0] = 0.7071;
-            y[1, 0] = 0.7660;
-            y[2, 0] = 0.8192;
-            y[3, 0] = 0.8660;
-            y[4, 0] = 0.9660;
-
-            // Hago las operaciones.
-            for (int i = 1; i < n; i++)
-            {
-                for (int j = 0; j < n - i; j++)
-                    y[j, i] = y[j + 1, i - 1] - y[j, i - 1];
-            }
-
-            // Dibujo el árbol.
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(x[i] + "\t");
-                for (int j = 0; j < n - i; j++)
-                    Console.Write(y[i, j] + "\t");
-                Console.WriteLine();
-            }
-
-            // Evalúo el polinomio en 52.
-            double valor = 52;
-
-
-
-            // initializing u and sum 
-            // Evalúa el punto.
-            double sum = y[0, 0];
-            double u = (valor - x[0]) / (x[1] - x[0]);
-            for (int i = 1; i < n; i++)
-            {
-                sum = sum + (u_cal(u, i) * y[0, i]) /
-                                        fact(i);
-            }
-
-            Console.WriteLine("\n Value at " + valor + " is " + Math.Round(sum, 6));
+            Console.WriteLine(resultado);
             Console.ReadKey();
         }
-
-        // calculating u mentioned in the formula 
-        static double u_cal(double u, int n)
-        {
-            double aux = u;
-            for (int i = 1; i < n; i++)
-                aux = aux * (u - i);
-            return aux;
-        }
-
-        // calculating factorial of given number n 
-        static int fact(int n)
-        {
-            int f = 1;
-            for (int i = 2; i <= n; i++)
-                f *= i;
-            return f;
-        }
-
-        #endregion - Private Methods -
     }
 }
---
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Negocio/NewtonGregory.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using Extreme.Mathematics.Curves;
+
+namespace UTN.MatematicaSuperior.Negocio
+{
+    public static class NewtonGregory
+    {
+        public static string Interpolar(List<double> xList, List<double> yList)
+        {
+            //Nos manejamos con arrays para hacer más eficientes las operaciones.
+
+            //n es la cantidad de puntos, da igual si se obtiene por xList o yList.
+            int n = xList.Count;
+
+            //Paso la lista de x a un array.
+            double[] x = xList.ToArray();
+
+            //Paso la lista de y a un array de nxn que va a contener el árbol.
+            double[,] y = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                y[i, 0] = xList[i];
+            }
+
+            // Hago las operaciones para generar el árbol.
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                    y[j, i] = (y[j + 1, i - 1] - y[j, i - 1]) / (x[i + j] - x[j]);
+
+                #region Pequeño analisis
+
+                //j es 0, i es 1
+                //y[0, 1] = (y[1, 0] - y[0, 0] / x[1] - x[0]
+
+                //j es 1, i es 1
+                //y[1, 1] = (y[2, 0] - y[1, 0] / x[2] - x[1]
+
+                //j es 2, i es 1
+                //y[2, 1] = (y[3, 0] - y[2, 0] / x[3] - x[2]
+
+                //j es 0, i es 2
+                //y[0, 2] = (y[1, 1] - y[0, 1] / x[2] - x[0]
+
+                //j es 1, i es 2
+                //y[1, 2] = (y[2, 1] - y[1, 1] / x[3] - x[1]
+
+
+                #endregion Pequeño analisis
+            }
+
+            //Para progresivo, los coeficientes van a estar en la fila y[0][]
+            //Para regresivo, van a estar en la recta y[n-1][0], y[n-2][1], ...
+
+            double[] coeficientes = ObtenerCoeficientesProgresivo(y, n);
+
+            //Creo un polinomio con el término independiente.
+            Polynomial polinomio = new Polynomial(new List<double> { coeficientes[0] });
+
+            //Itero por cada grado empezando del grado 1 (la x).
+            for (int i = 1; i < n; i++)
+            {
+                //Creo un polinomio temporal que va a contener los (x-a) multiplicados.
+                //Le asigno el primer (x-a) para evitar recursividad.
+                Polynomial polinomioTemp = new Polynomial(new List<double> { -coeficientes[1], 1 });
+
+                //Multiplico los demás (x-a), en caso de haber, al polinomio temporal.
+                for (int j = 1; j < i; j++)
+                {
+                    polinomioTemp *= new Polynomial(new List<double> { -coeficientes[j + 1], 1 });
+                }
+
+                //Le sumo al polinomio la multiplicación entre el término del árbol y el polinomio obtenido.
+                polinomio += new Polynomial(new List<double> { x[i] }) * polinomioTemp;
+            }
+
+
+            return polinomio.ToString();
+
+            // Dibujo el árbol.
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write(x[i] + "\t");
+            //    for (int j = 0; j < n - i; j++)
+            //        Console.Write(y[i, j] + "\t");
+            //    Console.WriteLine();
+            //}
+        }
+
+        private static double[] ObtenerCoeficientesProgresivo(double[,] arbol, int n)
+        {
+            double[] coeficientes = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                coeficientes[i] = arbol[0, n];
+            }
+
+            return coeficientes;
+        }
+    }
+}
