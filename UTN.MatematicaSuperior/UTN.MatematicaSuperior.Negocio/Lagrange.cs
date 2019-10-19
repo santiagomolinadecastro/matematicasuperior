c3925f0803985e96893ffcbe500c511853bee933
jmatiasschneider <jmatiasschneider@gmail.com>
2019-10-19 13:37:36 -0300

    Lagrange

---
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Negocio/Lagrange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Extreme.Mathematics.Curves;
+
+namespace UTN.MatematicaSuperior.Negocio
+{
+    public static class Lagrange
+    {
+        public static string Interpolar(List<double> xList, List<double> yList)
+        {
+            //Nos manejamos con arrays para hacer más eficientes las operaciones.
+
+            //n es el grado máximo del polinomio
+            int n = xList.Count - 1;
+
+            //Paso la lista de x a un array.
+            double[] x = xList.ToArray();
+
+            //Paso la lista de y a un array
+            double[] y = yList.ToArray();
+
+            // instancio el polinomio degrado n
+            Polynomial polinomio = new Polynomial(n);
+            polinomio[0] = 0;
+
+            Polynomial[] Lj = new Polynomial[n + 1]; // Cada uno de los polinomios base
+            Polynomial li;
+
+            // Por cada término calculo las bases polinómicas
+            for (int j = 0; j <= n; j++)
+            {
+                // Base
+                Lj[j] = new Polynomial(n);
+                Lj[j][0] = y[j];
+
+                for (int i = 0; i <= n; i++)
+                {
+                    if (i == j) continue; // Filtro
+
+                    // Voy multiplicando polinomios
+                    li = new Polynomial(1);
+                    li[0] = (-x[i]) / (x[j] - x[i]);
+                    li[1] = 1 / (x[j] - x[i]);
+
+                    Lj[j] = Lj[j] * li;
+                }
+
+                polinomio += Lj[j];
+            }
+
+            return polinomio.ToString();
+        }
+    }
+}
---
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Pruebas/Lagrange_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UTN.MatematicaSuperior.Negocio;
+
+namespace UTN.MatematicaSuperior.Pruebas
+{
+    [TestClass]
+    public class Lagrange_Tests
+    {
+        private List<double> xList, yList;
+
+        [TestMethod]
+        public void Lagrange_()
+        {
+            xList = new List<double> { -5, -1, 3, 5 };
+            yList = new List<double> { 333, -3, -83, -387 };
+
+            string interpolacion = Lagrange.Interpolar(xList, yList);
+
+            Assert.AreEqual("-3x^3-x^2+3x-2", interpolacion);
+        }
+    }
+}
