6edecdce113f86ff1ace342551110e021e09ac90
Santiago Molina de Castro <santiagomolinadecastro@gmail.com>
2019-10-19 09:41:03 -0300

    Evaluación del polinomio en un K.

---
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Negocio/Polinomio.cs
@@ -0,0 +1,33 @@
+﻿using Extreme.Mathematics.Curves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTN.MatematicaSuperior.Negocio
+{
+    public class Polinomio
+    {
+        Polynomial _polinomio;
+
+        public Polinomio(List<double> coeficientes)
+        {
+            _polinomio = new Polynomial(coeficientes.Count);
+
+            int index = 0;
+
+            foreach (var coeficiente in coeficientes)
+            {
+                _polinomio[index] = coeficiente;
+
+                index++;
+            }
+        }
+
+        public double EvaluarPolinomio(double k)
+        {
+            return _polinomio.ValueAt(k);
+        }
+    }
+}
---
+++ UTN.MatematicaSuperior/UTN.MatematicaSuperior.Pruebas/EvaluarPolinomio_Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTN.MatematicaSuperior.Negocio;
+
+namespace UTN.MatematicaSuperior.Pruebas
+{
+    [TestClass]
+    public class EvaluarPolinomio_Tests
+    {
+        [TestMethod]
+        public void T01_EvaluarEnTres()
+        {
+            List<double> coeficientes = new List<double>{ 2.3, 7.1, 1.1 };
+
+            var polinomio = new Polinomio(coeficientes);
+
+            var resultado = polinomio.EvaluarPolinomio(3);
+
+            Assert.IsTrue(resultado == 33.5);
+        }
+
+        [TestMethod]
+        public void T01_EvaluarEnCeroConTerminoIndependiente()
+        {
+            List<double> coeficientes = new List<double> { 2.3, 7.1, 1.1 };
+
+            var polinomio = new Polinomio(coeficientes);
+
+            var resultado = polinomio.EvaluarPolinomio(0);
+
+            Assert.IsTrue(resultado == 2.3);
+        }
+
+        [TestMethod]
+        public void T01_EvaluarEnCeroSinTerminoIndependiente()
+        {
+            List<double> coeficientes = new List<double> { 0, 7.1, 1.1 };
+
+            var polinomio = new Polinomio(coeficientes);
+
+            var resultado = polinomio.EvaluarPolinomio(0);
+
+            Assert.IsTrue(resultado == 0);
+        }
+    }
+}
